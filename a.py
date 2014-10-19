cf6f2446c3712e2cf21ba839ab5f36528e14e9e3
ronilp <ronilp3@gmail.com>
2014-10-20 02:00:52 +0530

    Working
    Just change invalid identifier to string literal

--- a.py
+++ a.py
@@ -2,31 +2,46 @@ import re
 
 tokenType = {
     'Nucleotide':'keyword','Protein':'keyword','Seq':'keyword','AminoAcids':'keyword','int':'keyword','for':'keyword','length':'keyword','in':'keyword','if':'keyword','else':'keyword','end':'keyword','print':'keyword','getSequence':'keyword','getAminoAcid':'keywords','to':'keyword','with':'keyword',
-    '-+-':'operator','-/-':'operator','.':'operator','(':'operator',')':'operator',',':'operator','{':'operator','}':'operator','[':'operator',']':'operator','+':'operator','-':'operator','*':'operator','/':'operator','++':'operator','--':'operator','%':'operator','==':'operator','>=':'operator','<=':'operator','>':'operator','<':'operator',';':'operator'
+    'is':'operator','-+-':'operator','-/-':'operator','+':'operator','-':'operator','*':'operator','/':'operator','++':'operator','--':'operator','==':'operator','>=':'operator','<=':'operator','>':'operator','<':'operator',
+    ';':'Special Symbol','(':'Special Symbol',')':'Special Symbol','%':'Special Symbol','{':'Special Symbol','}':'Special Symbol','[':'Special Symbol',']':'Special Symbol','.':'Special Symbol',',':'Special Symbol'
 }
 
 tokens = []
-literals = []
-identifier = []
-
 splittingOperators = ['(',')','}','{','[',']','+','-','.',',','*','/','%','==','>=','<=','>','<',';']
+ids = "[a-zA-z][a-zA-Z0-9]*|[a-zA-z]_[a-zA-Z0-9]"
 
-line = "hel>=l(ii %ei a) me*o/w -+- sk-/-k"
-x = re.split('(\\-\\+\\-|\\-\\/\\-|\\+\\+|\\+|\\-\\-|\\-|\s|\\(|\\)|\\{|\\}|\\[|\\]|\\.|\\*|\\%|==|\\>\\=|\\<\\=|;|/|>|<|\\=\\=|,)\s*',line)
-print (x)
+def isIntLiteral(integer):
+    try: 
+        int(integer)
+        return True
+    except ValueError:
+        return False
 
 def splitOperation(string):
-    pass
+    x = [value.strip() for value in re.split('(\s|\\-\\+\\-|\\-\\/\\-|\\+\\+|\\+|\\-\\-|\\-|\\(|\\)|\\{|\\}|\\[|\\]|\\.|\\*|\\%|==|\\>\\=|\\<\\=|;|/|>|<|\\=\\=|,)\s*',string) if not len(value.strip()) == 0]
+    return x
 
-file=open('3.txt')
-#for line in file:
-#    line=line.strip()
-#    if len(line) == 0:
-#        continue
-#    line=line.rstrip('\n')
-#    words=line.split()
-#    for word in words:
-#        tokens.extend(splitOperation(word))
+file=open('4.txt')
+for line in file:
+    line=line.strip()
+    if len(line) == 0:
+        continue
+    line=line.rstrip('\n')
+    tokens.extend(splitOperation(line))
 
-#print(tokens)
-#print ([tokenType[t] for t in tokens])
+for term in tokens:
+    if term in tokenType.keys():
+      print (term+' : '+tokenType[term])
+    elif isIntLiteral(term)==True:
+      print (term + ' : Integer Literal')
+    else:
+      tkn = re.findall(ids,term)
+      if(len(tkn) == 1 ):
+        if(tkn[0] == term):
+          print(term + ' : Identifier')
+        else:
+          print(term + ' : Invalid  Identifier')
+      else:
+        print(term + ' : Invalid  Identifier')
+
+
