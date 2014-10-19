dc273f04d20d2a250cf335000045e6f30ceb42b8
ronilp <ronilp3@gmail.com>
2014-10-20 00:56:42 +0530

    regex

--- a.py
+++ a.py
@@ -11,45 +11,22 @@ identifier = []
 
 splittingOperators = ['(',')','}','{','[',']','+','-','.',',','*','/','%','==','>=','<=','>','<',';']
 
-# x = re.split('(\\+\\+|\\+|\\-\\-|\\-|\s)\s*',line)
+line = "hel>=l(ii %ei a) me*o/w -+- sk-/-k"
+x = re.split('(\\-\\+\\-|\\-\\/\\-|\\+\\+|\\+|\\-\\-|\\-|\s|\\(|\\)|\\{|\\}|\\[|\\]|\\.|\\*|\\%|==|\\>\\=|\\<\\=|;|/|>|<|\\=\\=|,)\s*',line)
+print (x)
 
 def splitOperation(string):
-    temp=[]
-    templist2=[]
-    found=[]
-    
-    temp.append(string)
-
-    for symbol in splittingOperators:
-        for q in temp:
-            templist2=q.split(symbol)
-            tempans = []
-            for word in templist2:
-                tempans.append(word)
-                tempans.append(symbol)
-            templist2 = tempans[:-1]
-            found.extend(templist2)
-        found=[]
-
-     templist2 = []
-    for word in temp:
-        if not len(word) == 0:
-            templist2.append(word)
-    return templist2
+    pass
 
 file=open('3.txt')
-for line in file:
-    line=line.strip()
-    if len(line) == 0:
-        continue
-    line=line.rstrip('\n')
-    words=line.split()
-    for word in words:
-        tokens.extend(splitOperation(word))
+#for line in file:
+#    line=line.strip()
+#    if len(line) == 0:
+#        continue
+#    line=line.rstrip('\n')
+#    words=line.split()
+#    for word in words:
+#        tokens.extend(splitOperation(word))
 
-print(tokens)
-
-
+#print(tokens)
 #print ([tokenType[t] for t in tokens])
-
-
