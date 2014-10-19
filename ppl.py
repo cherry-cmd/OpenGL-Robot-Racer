539d3704a550de534e559bbc6e0ce82a8563790e
ronilp <ronilp3@gmail.com>
2014-10-19 23:28:07 +0530

    dict added

--- ppl.py
+++ ppl.py
@@ -1,21 +1,27 @@
 import re
 
-keywords=('Nucleotide','Protien','Seq','AminoAcids','int','for','length','in','if','else','end','print','getSequence','getAminoAcid','to','with')
-operators=('-+-','-/-','.','(',')',',','{','}','[',']','+','-','*','/','++','--','%','==','>=','<=','>','<',';')
+tokenType = {
+    'Nucleotide':'keyword','Protein':'keyword','Seq':'keyword','AminoAcids':'keyword','int':'keyword','for':'keyword','length':'keyword','in':'keyword','if':'keyword','else':'keyword','end':'keyword','print':'keyword','getSequence':'keyword','getAminoAcid':'keywords','to':'keyword','with':'keyword',
+    '-+-':'operator','-/-':'operator','.':'operator','(':'operator',')':'operator',',':'operator','{':'operator','}':'operator','[':'operator',']':'operator','+':'operator','-':'operator','*':'operator','/':'operator','++':'operator','--':'operator','%':'operator','==':'operator','>=':'operator','<=':'operator','>':'operator','<':'operator',';':'operator'
+}
 
-splittingOperators = ['(',')','}','{','[',']','+','-','.',',','*','/','++','--','%','==','>=','<=','>','<',';','-+-','-/-']
+tokens = []
+literals = []
+identifier = []
 
-x = re.split('(\\+\\+|\\+|\\-\\-|\\-|\s)\s*',line)
+splittingOperators = ['(',')','}','{','[',']','+','-','.',',','*','/','%','==','>=','<=','>','<',';']
+
+# x = re.split('(\\+\\+|\\+|\\-\\-|\\-|\s)\s*',line)
 
 def splitOperation(string):
-    templist=[]
+    temp=[]
     templist2=[]
     found=[]
     
-    templist.append(string)
+    temp.append(string)
 
     for symbol in splittingOperators:
-        for q in templist:
+        for q in temp:
             templist2=q.split(symbol)
             tempans = []
             for word in templist2:
@@ -23,17 +29,15 @@ def splitOperation(string):
                 tempans.append(symbol)
             templist2 = tempans[:-1]
             found.extend(templist2)
-        templist=found
         found=[]
 
     templist2 = []
-    for word in templist:
+    for word in temp:
         if not len(word) == 0:
             templist2.append(word)
     return templist2
 
 file=open('3.txt')
-tokens = []
 for line in file:
     line=line.strip()
     if len(line) == 0:
@@ -44,3 +48,8 @@ for line in file:
         tokens.extend(splitOperation(word))
 
 print(tokens)
+
+
+#print ([tokenType[t] for t in tokens])
+
+
